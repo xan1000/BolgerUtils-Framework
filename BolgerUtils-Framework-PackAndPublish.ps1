22dffb6da93e5abbdd03786375f1e36799c68c74
Matthew Bolger <xan1000@gmail.com>
2020-07-28 14:52:32 +1000

    Added readme and pack and publishing script.

---
+++ BolgerUtils-Framework-PackAndPublish.ps1
@@ -0,0 +1,142 @@
+Set-Variable ApiKey -Option Constant -Value (Get-Content -Raw ../ApiKey.txt)
+Set-Variable NuGetSource -Option Constant -Value "https://api.nuget.org/v3/index.json"
+Set-Variable MsBuildPath -Option Constant -Value `
+    "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MSBuild.exe"
+Set-Variable Options -Option Constant -Value @(
+    "BolgerUtils.Framework.EntityFramework",
+    "BolgerUtils.Framework.EnumDisplay",
+    "BolgerUtils.Framework.Razor",
+    "BolgerUtils.Framework.WebForms"
+)
+
+function Main {
+    while($true) {
+        for($i = 0; $i -lt $Options.length; $i++) {
+            Write-Host "$($i + 1)    -> $($Options[$i])"
+        }
+        Write-Host "Quit"
+        Write-Host
+
+        $input = SelectAnOption
+
+        if($input -ieq "quit") {
+            return
+        }
+
+        $index = 0
+        if(!([int]::TryParse($input, [ref] $index)) -or $index -lt 1 -or $index -gt $Options.length) {
+            InvalidInput
+            continue
+        }
+        
+        $index--
+        $option = $Options[$index]
+        MenuOption $option
+    }
+}
+
+function MenuOption {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $option
+    )
+
+    while($true) {
+        Write-Host "--- $($option) ---"
+        Write-Host "1    -> Pack"
+        Write-Host "2    -> Publish"
+        Write-Host "Back"
+        Write-Host
+
+        $input = SelectAnOption
+
+        if($input -eq "1") {
+            Pack $option
+
+            $input = SelectAnOption "Publish $($option) package now (yes)? "
+            if($input -ieq "yes") {
+                Publish $option $false
+            }
+        }
+        elseif($input -eq "2") {
+            Publish $option
+        }
+        elseif($input -ieq "back") {
+            return
+        }
+        else {
+            InvalidInput
+        }
+    }
+}
+
+function Pack {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $option
+    )
+
+    Write-Host "--- Packing: $($option) ---"
+    Write-Host
+
+    Remove-Item "$($option).*.nupkg"
+
+    $csprojPath = "./$($option)/$($option).csproj"
+
+    # https://stackoverflow.com/a/6338047
+    # https://stackoverflow.com/a/1674950
+    Invoke-Expression "& '$($MsBuildPath)' ./BolgerUtils.sln /t:$($option.Replace([char] '.', [char] '_'))"
+    nuget pack $csprojPath -IncludeReferencedProjects
+
+    Write-Host
+}
+
+function Publish {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $option,
+        [bool] $shouldConfirm = $true
+    )
+
+    Write-Host "--- Publishing: $($option) ---"
+    Write-Host
+
+    $packages = @(Get-Item "$($option).*.nupkg")
+    if($packages.Length -ne 1) {
+        throw
+    }
+
+    $packageName = $packages[0].Name
+    if($shouldConfirm) {
+        $input = SelectAnOption "Are you sure you want to publish the $($packageName) package (yes)? "
+        if($input -ine "yes") {
+            exit
+        }
+    }
+
+    nuget push $packageName $ApiKey -Source $NuGetSource
+    
+    Remove-Item $packageName
+    Write-Host
+    exit
+}
+
+function SelectAnOption {
+    param(
+        [string] $prompt = "Select an option: "
+    )
+
+    Write-Host -NoNewline $prompt
+    $input = Read-Host
+    Write-Host
+
+    return $input
+}
+
+function InvalidInput {
+    Write-Host "Invalid input."
+    Write-Host
+}
+
+# Entry point.
+Main
---
+++ Readme.md
@@ -0,0 +1,28 @@
+# BolgerUtils-Framework Repository
+
+BolgerUtils-Framework contains miscellaneous utilities available in various .NET NuGet packages aimed at C#.
+
+The packages available are:
+
+### .NET Framework 4.8
+
+* [BolgerUtils.Framework.EntityFramework](BolgerUtils.Framework.EntityFramework)
+* [BolgerUtils.Framework.EnumDisplay](BolgerUtils.Framework.EnumDisplay)
+* [BolgerUtils.Framework.Razor](BolgerUtils.Framework.Razor)
+* [BolgerUtils.Framework.WebForms](BolgerUtils.Framework.WebForms)
+
+Link to all these packages on NuGet:
+
+https://www.nuget.org/profiles/xan1000
+
+# Remarks
+
+For .NET Standard NuGet packages see:
+
+https://github.com/xan1000/BolgerUtils
+
+# Attribution
+
+The BolgerUtils icon is attributed to Bernadette O'Keefe:
+
+https://svg-clipart.com/sign/RsnsoX2-man-carrying-a-big-bag-of-money-clipart
